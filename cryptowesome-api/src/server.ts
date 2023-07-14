74a8491d5c3e36f9830285d0ca1937a033519bd0
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-07-14 20:42:18 +0200

    chore(api): cleaning express related files

--- cryptowesome-api/src/server.ts
+++ cryptowesome-api/src/server.ts
@@ -1,14 +1,3 @@
-import http from 'http';
-import expressApp from './app';
 import { PORT } from './config';
 
-/**
- * Creating HTTP server
- */
-const server = http.createServer(expressApp);
-
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-
-export default server;
+console.log("server file")
